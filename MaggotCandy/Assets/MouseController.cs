be2eade9666bdcf633aad7c807cddc94fce1cce0
Simon Sotak <4610874+the21st@users.noreply.github.com>
2014-01-26 14:24:46 +0100

    + fist falling mechanism (state machine)

--- MaggotCandy/Assets/MouseController.cs
+++ MaggotCandy/Assets/MouseController.cs
@@ -3,6 +3,22 @@ using System.Collections;
 
 public class MouseController : MonoBehaviour {
 
+	enum FistState
+	{
+		Idle,
+		Smashing,
+		Smashed
+	}
+
+	public float FallLength;
+	public float StaySmashed;
+
+	private float _currentFallLength;
+	private FistState _state;
+	private Vector2 _fallStartPosition;
+	private float _fallSpeed;
+	private float _smashedTime;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +26,52 @@ public class MouseController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition).To2D();
+		switch (_state)
+		{
+		case FistState.Idle:
+			transform.position = Extensions.To2D(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+			if (Input.GetMouseButtonDown(0))
+			{
+				_currentFallLength = 0;
+				_fallSpeed = 4;
+				_state = FistState.Smashing;
+				_fallStartPosition = transform.position;
+			}
+			break;
+		case FistState.Smashing:
+			if (_currentFallLength >= FallLength)
+			{
+				Smash();
+			}
+			else
+			{
+				var tempPos = transform.position;
+				var deltaFall = Time.deltaTime * _fallSpeed;
+				tempPos.y -= deltaFall;
+				_currentFallLength += deltaFall;
+				_fallSpeed += 100 * Time.deltaTime;
+				transform.position = tempPos;
+			}
+			break;
+		case FistState.Smashed:
+			if (_smashedTime >= StaySmashed)
+			{
+				_state = FistState.Idle;
+			}
+			else
+			{
+				_smashedTime += Time.deltaTime;
+			}
+			break;
+		default:
+			throw new System.ArgumentOutOfRangeException ();
+		}
+	}
+
+	void Smash ()
+	{
+		_state = FistState.Smashed;
+		_smashedTime = 0;
+		// TODO physics, breakup candy, etc.
 	}
 }
