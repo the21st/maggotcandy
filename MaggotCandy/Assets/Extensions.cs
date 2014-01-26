de1f2d78f0d99775c8b3489e43060005c20caa58
the21st <the21st@gmail.com>
2014-01-26 14:24:46 +0100

    + fist falling mechanism (state machine)

--- MaggotCandy/Assets/Extensions.cs
+++ MaggotCandy/Assets/Extensions.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-	public static class Extensions
+public static class Extensions
+{
+	public static Vector3 To2D(this Vector2 v)
 	{
-		public static Vector3 To2D(this Vector2 v)
-		{
-			return new Vector3(v.x, v.y, 0);
-		}
+		return new Vector3(v.x, v.y, 0);
 	}
+}
