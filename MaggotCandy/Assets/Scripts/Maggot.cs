1844a1ef872029f2c3626fc864ad475f8526ce67
the21st <the21st@gmail.com>
2014-02-01 18:15:18 +0100

    + maggot stub

---
+++ MaggotCandy/Assets/Scripts/Maggot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maggot : MonoBehaviour {
+
+	private bool _turnedRight;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_turnedRight = Random.Range (0, 2) == 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_turnedRight)
+		{
+			transform.position += new Vector3(Time.deltaTime, 0, 0);
+		}
+		else
+		{
+			transform.position += new Vector3(-Time.deltaTime, 0, 0);
+		}
+	}
+
+	void Crush(PushParams pushParams)
+	{
+		Destroy (gameObject);
+	}
+}
