fad657ecad27f3e9b7e74678c596a85c9c25b39f
Simon Sotak <4610874+the21st@users.noreply.github.com>
2014-02-01 16:21:45 +0100

    + stoned progress bar

--- MaggotCandy/Assets/StonedBar.cs
+++ MaggotCandy/Assets/StonedBar.cs
@@ -5,22 +5,30 @@ public class StonedBar : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		_progress = 0.2f;
 	}
 
-	public float progress = 0;
+	private static float _progress = 0;
 	public Vector2 pos  = new Vector2(20, 40);
 	public Vector2 size = new Vector2(20, 60);
 	public Texture2D progressBarEmpty;
 	public Texture2D progressBarFull;
-	
+
+	public static void Add(float x)
+	{
+		_progress += x;
+		_progress = Mathf.Clamp01 (_progress);
+	}
+
 	void OnGUI()
 	{
 		GUI.DrawTexture(new Rect(pos.x, pos.y, size.x, size.y), progressBarEmpty);
-		GUI.DrawTexture(new Rect(pos.x, pos.y + size.y * (1-Mathf.Clamp01(progress)), size.x, size.y * Mathf.Clamp01(progress)), progressBarFull);
+		GUI.DrawTexture(new Rect(pos.x, pos.y + size.y * (1-Mathf.Clamp01(_progress)), size.x, size.y * Mathf.Clamp01(_progress)), progressBarFull);
 	}
 	
 	void Update()
 	{
+		_progress -= 0.05f *  Time.deltaTime;
+		_progress = Mathf.Clamp01 (_progress);
 	}
 }
