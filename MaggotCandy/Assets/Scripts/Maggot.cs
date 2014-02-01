5e0819172939c20e7791fb65cb99a72daeaaada8
the21st <the21st@gmail.com>
2014-02-01 19:53:30 +0100

    + basic maggot movement and orientation math

--- MaggotCandy/Assets/Scripts/Maggot.cs
+++ MaggotCandy/Assets/Scripts/Maggot.cs
@@ -1,30 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Maggot : MonoBehaviour {
-
-	private bool _turnedRight;
-
+public class Maggot : MonoBehaviour
+{
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-		_turnedRight = Random.Range (0, 2) == 0;
+		transform.localRotation = Quaternion.AngleAxis (Random.Range (0, 360f), new Vector3 (0, 0, 1));
 	}
-	
+    
 	// Update is called once per frame
-	void Update () {
-		if (_turnedRight)
+	void Update()
+	{
+		var direction = this.transform.right;
+
+		if (Vector3.Dot(transform.up, Vector3.up) < 0)
 		{
-			transform.position += new Vector3(Time.deltaTime, 0, 0);
-		}
-		else
+			transform.localScale = new Vector3(1, -1, 1);
+		} else
 		{
-			transform.position += new Vector3(-Time.deltaTime, 0, 0);
+			transform.localScale = new Vector3(1, 1, 1);
 		}
+
+		transform.position += Time.deltaTime * direction;
+
+		transform.Rotate(new Vector3 (0, 0, 1), -1);
 	}
 
 	void Crush(PushParams pushParams)
 	{
-		Destroy (gameObject);
+		Destroy(gameObject);
 	}
 }
