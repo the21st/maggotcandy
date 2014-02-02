6bb88030549a28e247074c312b8b67caac94692d
Simon Sotak <4610874+the21st@users.noreply.github.com>
2014-02-02 12:04:37 +0100

    Spawned stuff is falling from top

---
+++ MaggotCandy/Assets/Scripts/FallingSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingSpawner : MonoBehaviour
+{
+	public Vector2 Target;
+	public GameObject PrefabToCreate;
+
+	private bool _falling;
+
+	// Use this for initialization
+	void Start()
+	{
+		GetComponent<SpriteRenderer>().sprite = PrefabToCreate.GetComponent<SpriteRenderer>().sprite;
+
+		_falling = rigidbody2D.gravityScale > 0;
+
+		if (_falling)
+		{
+			transform.position = (Target + new Vector2(0, 5)).To3D();
+		} else
+		{
+			transform.position = (Target + new Vector2(0, -5)).To3D();
+		}
+
+		rigidbody2D.AddTorque(3000);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		var height = _falling ? transform.position.y : -transform.position.y;
+
+		if (height < Target.y)
+		{
+			Instantiate(PrefabToCreate, transform.position, transform.rotation);
+
+			Destroy(gameObject);
+		}
+	}
+}
