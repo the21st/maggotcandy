1052b9ded98536602462e4df32491e57e9e21d10
the21st <the21st@gmail.com>
2014-02-02 12:04:37 +0100

    Spawned stuff is falling from top

--- MaggotCandy/Assets/Scripts/Spawner.cs
+++ MaggotCandy/Assets/Scripts/Spawner.cs
@@ -2,28 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
+	public GameObject FallingObject;
 
 	public List<GameObject> CandyToSpawn;
 	public List<GameObject> MaggotsToSpawn;
-
 	public float CandyFallPeriod = 3;
 	public float SneezePeriod = 7;
-
 	private float _candyFallTimer;
 	private float _sneezeTimer;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 	
 	}
 
-	Vector3 GenerateSpawnPos()
+	Vector2 GenerateSpawnPos()
 	{
 		var boxCollider = GetComponent<BoxCollider2D>();
 		var x = Random.Range(-0.5f * boxCollider.size.x, 0.5f * boxCollider.size.x);
 		var y = Random.Range(-0.5f * boxCollider.size.y, 0.5f * boxCollider.size.y);
-		var position = transform.position + new Vector3(x, y, 0);
+		var position = transform.position.To2D() + new Vector2(x, y);
 		return position;
 	}
 
@@ -33,18 +34,24 @@ public class Spawner : MonoBehaviour {
 
 		var position = GenerateSpawnPos();
 
-		Instantiate(MaggotsToSpawn.GetRandomElement(), position, Quaternion.identity);
+		var fallingObject = (GameObject) Instantiate(FallingObject);
+		var fallingScript = fallingObject.GetComponent<FallingSpawner>();
+		fallingScript.PrefabToCreate = MaggotsToSpawn.GetRandomElement();
+		fallingScript.Target = position;
 	}
 
 	void SpawnCandy()
 	{
 		var position = GenerateSpawnPos();
-		
-		Instantiate(CandyToSpawn.GetRandomElement(), position, Quaternion.identity);
+
+		var fallingObject = (GameObject) Instantiate(FallingObject);
+		var fallingScript = fallingObject.GetComponent<FallingSpawner>();
+		fallingScript.PrefabToCreate = CandyToSpawn.GetRandomElement();
+		fallingScript.Target = position;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
 		_candyFallTimer += Time.deltaTime;
 		if (_candyFallTimer > CandyFallPeriod)
